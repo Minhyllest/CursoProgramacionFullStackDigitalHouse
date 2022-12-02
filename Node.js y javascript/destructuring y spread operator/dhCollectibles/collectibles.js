f46db7cd0702402f4cc11a81fb05f36a197ab414
Bruno Fabián Cabral <minhyllest@gmail.com>
2022-12-02 20:22:49 -0300

    Destructuring y spread operator hecho

--- Node.js y javascript/destructuring y spread operator/dhCollectibles/collectibles.js
+++ Node.js y javascript/destructuring y spread operator/dhCollectibles/collectibles.js
@@ -1,9 +1,25 @@
 let fs = require("fs")
-const bicicletasfile = fs.readFileSync(__dirname + '/datos', 'utf-8');
-const bicicletas = JSON.parse(bicicletasfile);
 
-module.exports = bicicletas
+function importar(marca){
+    let archivo = "";
+    switch (marca){
+        case "Hot Toys":
+            archivo = "figuras1.json"
+            break;
+        case "Bandai":
+            archivo = "figuras2.json"
+            break;
+        case "Star Wars":
+            archivo = "figuras3.json"
+            break;
+        default :
+            break;
+    }
+    let rutaArchivo = "./datos/" + archivo;
 
-function importar(...marca){
-    
+    let figuras = JSON.parse(fs.readFileSync(rutaArchivo, "utf-8"));
+
+    return figuras
 }
+module.exports = importar
+
